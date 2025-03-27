55d1c86d2dd1f36e124f20ebf753187e7ffa1b12
danielkasai <danielkasai2012@gmail.com>
2025-03-27 11:16:00 +0100

    EveryLayout as styles, not components (copy paste from site)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import './components/EveryLayout/import_all.js';
+// import './components/EveryLayout/import_all.js';
 
 import { registerAvatarComponent } from './components/avatar/avatar.js';
 const app = () => {
