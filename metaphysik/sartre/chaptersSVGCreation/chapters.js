90855e2b6f52e3ce162c575859e7ee9a031f5a13
danielkasai <daniel.kasai@funkemedien.de>
2025-07-01 13:03:55 +0200

    Sartre Metaphysik > chapters.js - completion

--- metaphysik/sartre/chaptersSVGCreation/chapters.js
+++ metaphysik/sartre/chaptersSVGCreation/chapters.js
@@ -59,7 +59,6 @@ export default [
             "title": "V. Der Ursprung des Nichts",
             "height": 400
           }
-        
         ]
       },
       {
@@ -114,27 +113,192 @@ export default [
         "sections": [
           {
             "title": "I. Phänomenologie der drei zeitlichen Dimensionen: A) Die Vergangenheit",
-            "height": 100
+            "height": 400
           },
           {
             "title": "B) Die Gegenwart",
-            "height": 100
+            "height": 400
           },
           {
             "title": "C) Die Zukunft",
-            "height": 100
+            "height": 400
           },
           {
             "title": "II. Ontologie der Zeitlichkeit: A) Die statische Zeitlichkeit",
-            "height": 100
+            "height": 400
           },
           {
             "title": "B) Dynamik der Zeitlichkeit",
-            "height": 100
+            "height": 400
           },
           {
             "title": "Ursprüngliche Zeitlichkeit und psychische Zeitlichkeit: Die Reflexion",
-            "height": 100
+            "height": 400
+          }
+        ]
+      },
+      {
+        "title": "Drittes Kapitel: Die Transzendenz",
+        "sections": [
+          {
+            "title": "I. Die Erkenntnis als Beziehungstypus zwischen dem Für-sich und dem An-sich",
+            "height": 400
+          },
+          {
+            "title": "II. Über die Bestimmung als Negation",
+            "height": 400
+          },
+          {
+            "title": "III. Qualität und Quantität, Potentialität, Utensilität",
+            "height": 400
+          },
+          {
+            "title": "IV. Die Weltzeit: A) Die Vergangenheit",
+            "height": 400
+          },
+          {
+            "title": "B) Die Gegenwart",
+            "height": 400
+          },
+          {
+            "title": "C) Die Zukunft",
+            "height": 400
+          },
+          {
+            "title": "V. Die Erkenntnis",
+            "height": 400
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "title": "Dritter Teil: Das Für-Andere",
+    "chapters": [
+      {
+        "title": "Erstes Kapitel: Die Existenz Anderer",
+        "sections": [
+          {
+            "title": "I. Das Problem",
+            "height": 400
+          },
+          {
+            "title": "II. Die Klippe des Solipsismus",
+            "height": 400
+          },
+          {
+            "title": "III. Husserl, Hegel, Heidegger",
+            "height": 400
+          },
+          {
+            "title": "IV. Der Blick",
+            "height": 400
+          }
+        ]
+      },
+      {
+        "title": "Zweites Kapitel: Der Körper",
+        "sections": [
+          {
+            "title": "I. Der Körper als Für-sich-sein: Die Faktizität",
+            "height": 400
+          },
+          {
+            "title": "II. Der Körper-für-andere",
+            "height": 400
+          },
+          {
+            "title": "III. Die dritte ontologische Dimension des Körpers",
+            "height": 400
+          }
+        ]
+      },
+      {
+        "title": "Drittes Kapitel: Die konkreten Beziehungen zu Anderen",
+        "sections": [
+          {
+            "title": "I. Die erste Haltung gegenüber Anderen: die Liebe, die Sprache, der Masochismus",
+            "height": 400
+          },
+          {
+            "title": "II. Zweite Haltung gegenüber Anderen: die Gleichgültigkeit, die Begierde, der Hass, der Sadismus",
+            "height": 400
+          },
+          {
+            "title": "III. Das 'Mitsein' und das 'Wir': A) Das Objekt-Wir",
+            "height": 400
+          },
+          {
+            "title": "B) Das Subjekt-Wir",
+            "height": 400
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "title": "Vierter Teil: Haben, Handeln und Sein",
+    "chapters": [
+      {
+        "title": "Erstes Kapitel: Sein und Handeln: Die Freiheit",
+        "sections": [
+          {
+            "title": "I. Die erste Bedingung des Handelns ist die Freiheit",
+            "height": 400
+          },
+          {
+            "title": "II. Freiheit und Faktizität: Die Situation: A) Mein Platz",
+            "height": 400
+          },
+          {
+            "title": "B) Meine Vergangenheit",
+            "height": 400
+          },
+          {
+            "title": "C) Meine Umgebung",
+            "height": 400
+          },
+          {
+            "title": "D) Mein Nächster",
+            "height": 400
+          },
+          {
+            "title": "E) Mein Tod",
+            "height": 400
+          },
+          {
+            "title": "III. Freiheit und Verantwortlichkeit",
+            "height": 400
+          }
+        ]
+      },
+      {
+        "title": "Zweites Kapitel: Handeln und Haben",
+        "sections": [
+          {
+            "title": "I. Die existentielle Psychoanalyse",
+            "height": 400
+          },
+          {
+            "title": "II. Handeln und Haben: Der Besitz",
+            "height": 400
+          },
+          {
+            "title": "III. Übe rdie Qualität als seinsenthüllend",
+            "height": 400
+          }
+        ]
+      },
+      {
+        "title": "Schlussfolgerungen",
+        "sections": [
+          {
+            "title": "I. An-sich und Für-sich: Metaphysische Apercus",
+            "height": 400
+          },
+          {
+            "title": "II. Moralische Perspektiven",
+            "height": 400
           }
         ]
       }
