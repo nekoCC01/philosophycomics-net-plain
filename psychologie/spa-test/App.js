0d7ed8ecb923c33086fd0b8aaf36fd129648f841
danielkasai <daniel.kasai@funkemedien.de>
2025-07-07 15:39:27 +0200

    spa ex > extract to template

--- psychologie/spa-test/App.js
+++ psychologie/spa-test/App.js
@@ -1,35 +1,8 @@
 
 class App extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `
-        <h1>Basic Example</h1>
-
-        <p>
-            This example demonstrates how the features of a framework router
-            can be approximated using web components and a vanilla hash router.
-        </p>
-        <p>
-            Check out the original <a href="https://github.com/remix-run/react-router/tree/dev/examples/basic" target="_blank">React Router basic example</a> for comparison.
-        </p>
-
-        <!-- Routes can be nested, but the path must always be fully specified. -->
-        <x-route path="/">
-            <x-app-layout></x-app-layout>
-            <x-route path="/" exact>
-                <h2>Home</h2>
-            </x-route>
-            <x-route path="/about" exact>
-                <h2>About</h2>
-            </x-route>
-            <x-route path="/dashboard" exact>
-                <h2>Dashboard</h2>
-            </x-route>
-            <x-route path="*">
-                <h2>Nothing to see here</h2>
-                <a href="#/">Go to the home page</a>
-            </x-route>
-        </x-route>
-        `;
+        let template = document.querySelector('template#app')
+        this.appendChild(template.content.cloneNode(true));
     }
 }
 
