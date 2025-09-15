9727763ba68ab63e5a5ff32b4151b23ad49f47eb
danielkasai <daniel.kasai@funkemedien.de>
2025-09-15 22:28:25 +0200

    Riemann-Thomann > input flow - storage for person1

--- psychologie/spa-test/components/Test.js
+++ psychologie/spa-test/components/Test.js
@@ -1,4 +1,4 @@
-import { testImport } from '../storage.js';
+import { loadAnswers, addPerson, saveAnswer, getPersonKeys } from '../storage.js';
 
 class Test extends HTMLElement {
     async connectedCallback() {
@@ -23,7 +23,8 @@ class Test extends HTMLElement {
                 let fragmentQuestion = templateQuestion.content.cloneNode(true);
                 fragmentQuestion.querySelector('p').textContent = question.text;
                 fragmentQuestion.querySelectorAll('input').forEach(input => {
-                    input.name = question.id;
+                    // erstmal hard-coded 
+                    input.name = 'person1-' + question.id;
                 })
                 rootQuestions.appendChild(fragmentQuestion);
             })
@@ -32,26 +33,47 @@ class Test extends HTMLElement {
 
         })
 
-        fragmentTest.querySelectorAll('input').forEach(radio => {
+
+        // Personen Input
+        fragmentTest.querySelectorAll('input.persons-input').forEach(radio => {
+            radio.addEventListener('change', e => {
+                console.log("Ausgewählt P", e.target, e.target.value, e.target.name);
+                if (e.target.value == 2) {
+                    this.querySelectorAll('.name-input').forEach(input => {
+                        input.style.display = "block";
+                    })
+                } else {
+                    this.querySelector('#name-input').style.display = "block";
+                }
+                this.querySelector('#persons').style.display = "none";
+            })
+        })
+
+        // Namen Input
+        let nameInput = fragmentTest.getElementById('name-input');
+        nameInput.addEventListener("submit", (event) => {
+            event.preventDefault(); // verhindert das automatische Neuladen
+            const inputValue = nameInput.elements["username"].value;
+            console.log("Finaler Wert:", inputValue);
+            // an Storage
+            addPerson();
+            this.querySelectorAll('.name-input').forEach(input => {
+                input.style.display = "none";
+            })
+            this.querySelector('#dimensions').style.display = "block";
+
+        });
+
+        // Antworten Input
+        fragmentTest.querySelectorAll('input.answer-input').forEach(radio => {
             radio.addEventListener('change', e => {
                 console.log("Ausgewählt", e.target, e.target.value, e.target.name);
+                let [personKey, questionId] = e.target.name.split("-");
+                saveAnswer(personKey, questionId, e.target.value);
             })
         })
 
         this.appendChild(fragmentTest);
-
-        /*
-        // Ziel-Container im Template finden
-        const listContainer = this.querySelector('#meine-liste');
-        daten.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = item.name;
-            listContainer.appendChild(li);
-        });
-
-        //Test
-        console.log(testImport());
-        */
 
     }
 }
--- psychologie/spa-test/storage.js
+++ psychologie/spa-test/storage.js
@@ -1,3 +1,25 @@
-export function testImport() {
-    return 'returnValue';
+const STORAGE_KEY = 'riemann_test';
+
+const PERSON_KEYS = ['person1', 'person2'];
+
+export function loadAnswers() {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY)) || { person1: {} };
+}
+
+export function saveAnswer(personKey, id, value) {
+    const current = loadAnswers();
+    if (!current[personKey]) current[personKey] = {};
+    current[personKey][id] = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(current));
+}
+
+export function addPerson() {
+    const answers = loadAnswers();
+    if (!answers.person2) answers.person2 = {};
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(answers));
+}
+
+export function getPersonKeys() {
+    const answers = loadAnswers();
+    return Object.keys(answers);
 }
