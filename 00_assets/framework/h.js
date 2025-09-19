d5c73f4f5790a30e72d2df032b55c14f0b82afcb
danielkasai <daniel.kasai@funkemedien.de>
2025-09-19 07:07:27 +0200

    FWF > start coding

---
+++ 00_assets/framework/h.js
@@ -0,0 +1,16 @@
+import { withoutNulls } from './utils/arrays';
+
+export const DOM_TYPES = {
+  TEXT: 'text',
+  ELEMENT: 'element',
+  FRAGMENT: 'fragment',
+}
+
+export function h(tag, props = {}, children = []) {
+    return {
+        tag,
+        props,
+        children: mapTextNodes(withoutNulls(children)),
+        type: DOM_TYPES.ELEMENT
+    };
+}
