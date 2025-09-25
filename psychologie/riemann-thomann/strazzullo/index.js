66a3f0a3bfe649e4840c95b20c0b3e08fde0d3b2
danielkasai <daniel.kasai@funkemedien.de>
2025-09-25 11:41:56 +0200

    RT - strazz > copy first iteration code

--- psychologie/riemann-thomann/strazzullo/index.js
+++ psychologie/riemann-thomann/strazzullo/index.js
@@ -1,12 +1,12 @@
-import loadQuestions from './questions.js'
+import getTodos from './getTodos.js'
 import view from './view.js'
 
 const state = {
-    questions: loadQuestions(),
-    answers: {}
+    todos: getTodos(),
+    currentFilter: 'All'
 }
 
-const main = document.querySelector('#app')
+const main = document.querySelector('.todoapp')
 
 window.requestAnimationFrame(() => {
     const newMain = view(main, state)
