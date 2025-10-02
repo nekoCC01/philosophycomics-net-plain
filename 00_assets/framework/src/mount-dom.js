4d9ebebca8f833584e6d8091db8d723554d2d002
danielkasai <daniel.kasai@funkemedien.de>
2025-10-02 19:49:30 +0200

    FWF > mount-dom.js

---
+++ 00_assets/framework/src/mount-dom.js
@@ -0,0 +1,43 @@
+
+import { DOM_TYPES } from './h'
+
+export function mountDOM(vdom, parentEl) {
+  switch (vdom.type) {
+    case DOM_TYPES.TEXT: {
+      createTextNode(vdom, parentEl)
+      break
+    }
+
+    case DOM_TYPES.ELEMENT: {
+      createElementNode(vdom, parentEl)
+      break
+    }
+
+    case DOM_TYPES.FRAGMENT: {
+      createFragmentNodes(vdom, parentEl)
+      break
+    }
+
+    default: {
+      throw new Error(`Can't mount DOM of type: ${vdom.type}`)
+    }
+  }
+}
+
+function createTextNode(vdom, parentEl) {
+  const { value } = vdom
+
+  const textNode = document.createTextNode(value)
+  vdom.el = textNode
+
+  parentEl.append(textNode)
+}
+
+// TODO: implement createElementNode()
+
+function createFragmentNodes(vdom, parentEl) {
+  const { children } = vdom
+  vdom.el = parentEl
+
+  children.forEach((child) => mountDOM(child, parentEl))
+}
