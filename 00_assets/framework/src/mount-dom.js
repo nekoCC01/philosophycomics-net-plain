cb8fab14018a53ada26c8f25555e005bf5ff5195
danielkasai <daniel.kasai@funkemedien.de>
2025-10-09 10:53:38 +0200

    FWF > finish mount-dom module (destroy)

--- 00_assets/framework/src/mount-dom.js
+++ 00_assets/framework/src/mount-dom.js
@@ -2,6 +2,7 @@
 import { DOM_TYPES } from './h.js'
 import { setAttributes } from './attributes.js'
 import { addEventListeners } from './events.js'
+import { removeEventListeners } from './events'
 
 export function mountDOM(vdom, parentEl) {
   switch (vdom.type) {
@@ -58,4 +59,53 @@ function addProps(el, props, vdom) {
 
   vdom.listeners = addEventListeners(events, el)
   setAttributes(el, attrs)
+}
+
+export function destroyDOM(vdom) {
+  const { type } = vdom
+
+  switch (type) {
+    case DOM_TYPES.TEXT: {
+      removeTextNode(vdom)
+      break
+    }
+
+    case DOM_TYPES.ELEMENT: {
+      removeElementNode(vdom)
+      break
+    }
+
+    case DOM_TYPES.FRAGMENT: {
+      removeFragmentNodes(vdom)
+      break
+    }
+
+    default: {
+      throw new Error(`Can't destroy DOM of type: ${type}`)
+    }
+  }
+
+  delete vdom.el
+}
+
+function removeTextNode(vdom) {
+  const { el } = vdom
+  el.remove()
+}
+
+function removeElementNode(vdom) {
+  const { el, children, listeners } = vdom
+
+  el.remove()
+  children.forEach(destroyDOM)
+
+  if (listeners) {
+    removeEventListeners(listeners, el)
+    delete vdom.listeners
+  }
+}
+
+function removeFragmentNodes(vdom) {
+  const { children } = vdom
+  children.forEach(destroyDOM)
 }
