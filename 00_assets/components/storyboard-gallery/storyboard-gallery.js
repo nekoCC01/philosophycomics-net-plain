3a3db559059e243fe8aba7e3d0b1942e78d2d622
danielkasai <daniel.kasai@funkemedien.de>
2025-10-15 21:45:24 +0200

    StoryboardGallery > simple shadowDOM

--- 00_assets/components/storyboard-gallery/storyboard-gallery.js
+++ 00_assets/components/storyboard-gallery/storyboard-gallery.js
@@ -1,7 +1,28 @@
 
 class StoryboardGallery extends HTMLElement {
     connectedCallback() {
-        this.textContent = 'hello world!';
+        // Shadow DOM anlegen
+        if (!this.shadowRoot) {
+            const shadow = this.attachShadow({ mode: 'open' });
+            shadow.innerHTML = `
+                <style>
+                    :host {
+                        display: block;
+                    }
+                    .gallery {
+                        display: flex;
+                        flex-direction: row;
+                        gap: 2rem;
+                        overflow-x: auto;
+                        align-items: flex-start;
+                    }
+                </style>
+                <div class="gallery">
+                    <slot></slot>
+                </div>
+            `;
+        }
     }
 }
+
 customElements.define('storyboard-gallery', StoryboardGallery);
