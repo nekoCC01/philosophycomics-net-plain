886ac560f1567001e5e2e48b0cdfa0346074a560
danielkasai <daniel.kasai@funkemedien.de>
2025-11-09 16:30:59 +0100

    RT - strazz > input as template

--- psychologie/riemann-thomann/strazzullo/view/input.js
+++ psychologie/riemann-thomann/strazzullo/view/input.js
@@ -1,24 +1,19 @@
+let template;
+
+const getInputTemplate = () => {
+    if (!template) {
+        template = document.getElementById('inputTemplate');
+    }
+    return template
+        .content
+        .cloneNode(true)
+}
+
 export default (targetElement, { persons }) => {
     const newInput = targetElement.cloneNode(true)
 
-    newInput.innerHTML = `
-            <h3>${persons}</h3>
-            <fieldset id="persons">
-                <legend>Wieviel Teilnehmer?</legend>
-                <input class="persons-input" type="radio" name="persons" value="1" id="p1">
-                <label for="p1">1 Person</label>
-                <input class="persons-input" type="radio" name="persons" value="2" id="p2">
-                <label for="p2">2 Personen</label>
-            </fieldset>
-
-            <form class="name-input" id="name-input" action="">
-                <input type="text" name="username" placeholder="Namen eingeben">
-                <button type="submit">Abschicken</button>
-            </form>
-            <form class="name-input" id="name-input2" action="">
-                <input type="text" name="username2" placeholder="Namen eingeben">
-                <button type="submit">Abschicken</button>
-            </form>
-  `;
+    let inputTemplate = getInputTemplate();
+    inputTemplate.querySelector('h3').textContent = persons;
+    newInput.appendChild(inputTemplate);
     return newInput
 }
