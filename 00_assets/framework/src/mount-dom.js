c82a0cab36481bbdedae7d09600d9f89be1726a8
danielkasai <daniel.kasai@funkemedien.de>
2025-12-01 22:09:39 +0100

    FWF > dispatcher

--- 00_assets/framework/src/mount-dom.js
+++ 00_assets/framework/src/mount-dom.js
@@ -2,7 +2,6 @@
 import { DOM_TYPES } from './h.js'
 import { setAttributes } from './attributes.js'
 import { addEventListeners } from './events.js'
-import { removeEventListeners } from './events'
 
 export function mountDOM(vdom, parentEl) {
   switch (vdom.type) {
@@ -61,51 +60,3 @@ function addProps(el, props, vdom) {
   setAttributes(el, attrs)
 }
 
-export function destroyDOM(vdom) {
-  const { type } = vdom
-
-  switch (type) {
-    case DOM_TYPES.TEXT: {
-      removeTextNode(vdom)
-      break
-    }
-
-    case DOM_TYPES.ELEMENT: {
-      removeElementNode(vdom)
-      break
-    }
-
-    case DOM_TYPES.FRAGMENT: {
-      removeFragmentNodes(vdom)
-      break
-    }
-
-    default: {
-      throw new Error(`Can't destroy DOM of type: ${type}`)
-    }
-  }
-
-  delete vdom.el
-}
-
-function removeTextNode(vdom) {
-  const { el } = vdom
-  el.remove()
-}
-
-function removeElementNode(vdom) {
-  const { el, children, listeners } = vdom
-
-  el.remove()
-  children.forEach(destroyDOM)
-
-  if (listeners) {
-    removeEventListeners(listeners, el)
-    delete vdom.listeners
-  }
-}
-
-function removeFragmentNodes(vdom) {
-  const { children } = vdom
-  children.forEach(destroyDOM)
-}
