182f1878dc6e575bdeeed3aee8767f3d6279df7a
danielkasai <daniel.kasai@funkemedien.de>
2026-05-29 18:36:24 +0200

    adjust index.css index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // import './00_assets/components/EveryLayout/import_all.js';
 
+console.log("index.js loaded");
+
 import { registerFooterComponent } from './00_assets/components/Footer.js';
 const app = () => {
     registerFooterComponent();
